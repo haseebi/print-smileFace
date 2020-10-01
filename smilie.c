5e76c00e0832bb8e03ef2fd331d01a5044cccd1a
SAPNA UROOJ IQBAL <55201113+sapnauroojiqbal@users.noreply.github.com>
2020-10-01 10:58:23 +0500

    Update smilie.c

--- smilie.c
+++ smilie.c
@@ -8,9 +8,13 @@ void main()
 	int sml=1, i, limit;
 	printf("How many smiley face you want to print ? ");
 	scanf("%d",&limit);
+	printf("So");
 	for(i=0; i<limit; i++)
 	{
 		printf("%c ",sml);
 	}
+	printf("are the ");
+	printf("%d",&limit);
+	printf(" smiley face.");
 	getch();
 }
